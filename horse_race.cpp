da57e97d6d31028af847b383ad769326c149cf37
ajbeck800 <ajbeck800@gmail.com>
2025-09-11 19:15:18 +0000

    Updated horse_race.cpp

--- horse_race.cpp
+++ horse_race.cpp
@@ -1,9 +1,28 @@
-
-
 #include <random>
-std::random_device rd;
-std::uniform_int_distribution<int> dist(0, 1);
 
-coin = dist(rd);
+const int NUM_HORSES;
+const int TRACK_LENGTH;
 
+void advance(int, int*);
+void printLane(int, int*);
+bool isWinner(int, int*);
+
+int main(){
+
+} // end main
+
+void advance(int horseNum, int* horses){
+	std::random_device rd;
+	std::uniform_int_distribution<int> dist(0, 1);
+	
+	coin = dist(rd);
+} // end advance
+
+void printLane(int horseNum, int* horses){
+
+} // end printLane
+
+bool isWinner(int horseNum, int* horses){
+
+} // end isWinner
 
